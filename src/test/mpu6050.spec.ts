330f0bc9140d9c1752782dafc4be52ea271fa0af
Douglas Reuter <douglas.reuter@egov.com>
2020-01-10 10:59:05 -0600

    test

--- src/test/mpu6050.spec.ts
+++ src/test/mpu6050.spec.ts
@@ -1,4 +1,4 @@
-import { mpu6050, mpu6050Address, mpu6050Register } from "../devices/i2c/mpu6050";
+import { mpu6050, mpu6050Address, mpu6050Register, mpu6050ClockSource } from "../devices/i2c/mpu6050";
 
 describe(' mpu6050 ', function() {
    const mpu = new mpu6050(mpu6050Address.A)
@@ -20,6 +20,41 @@ describe(' mpu6050 ', function() {
 
         console.log({ intialSleepEnabled,oppositeSleeEnabled,newSleepEnabled})
       expect(newSleepEnabled).toBe(oppositeSleeEnabled);
+      done();
+    });
+    it('i2CMasterModeEnabled test', async function(done) {
+        const test = 255;
+      
+        const intiali2CMasterModeEnabled = mpu.i2CMasterModeEnabled
+        const oppositei2CMasterModeEnabled = !intiali2CMasterModeEnabled;
+        mpu.i2CMasterModeEnabled = oppositei2CMasterModeEnabled;
+        const newintiali2CMasterModeEnabled = mpu.i2CMasterModeEnabled;
+
+        console.log({ intiali2CMasterModeEnabled,oppositei2CMasterModeEnabled,newintiali2CMasterModeEnabled})
+      expect(newintiali2CMasterModeEnabled).toBe(oppositei2CMasterModeEnabled);
+      done();
+    });
+    it('i2CBypassEnabled test', async function(done) {
+        const test = 255;
+      
+        const intiali2CBypassEnabled = mpu.i2CBypassEnabled
+        const oppositei2CBypassEnabled = !intiali2CBypassEnabled;
+        mpu.i2CBypassEnabled = oppositei2CBypassEnabled;
+        const newintiali2CBypassEnabled = mpu.i2CBypassEnabled
+        console.log({ intiali2CBypassEnabled,oppositei2CBypassEnabled,newintiali2CBypassEnabled})
+      expect(newintiali2CBypassEnabled).toBe(oppositei2CBypassEnabled);
+      done();
+    });
+
+    it('_clockSource test', async function(done) {
+        const test = 255;
+      
+        const intial = mpu.clockSource;
+        const opposite = intial=== mpu6050ClockSource.PLLwithZGyroReference ? mpu6050ClockSource.PLLwithXGyroReference : mpu6050ClockSource.PLLwithZGyroReference;
+        mpu.clockSource = opposite;
+        const newv = mpu.clockSource
+        console.log({ intial,opposite,newv})
+      expect(newv).toBe(opposite);
       done();
     });
   });
