7cafc5b163e67cec29395f7d20fac87522d8b830
Douglas Reuter <douglas.reuter@egov.com>
2020-01-10 10:37:25 -0600

    sleep enabled test

--- src/test/mpu6050.spec.ts
+++ src/test/mpu6050.spec.ts
@@ -2,11 +2,21 @@ import { mpu6050, mpu6050Address } from "../devices/i2c/mpu6050";
 
 describe(' mpu6050 ', function() {
    const mpu = new mpu6050(mpu6050Address.A)
-    console.log('hi');
+ 
     it('getDeviceId should be 0x68', async function(done) {
-        const r = mpu.getDeviceId();
+        const deviceId = mpu.getDeviceId();
+        const expectedId = 0x68;
+        console.log({deviceId,expectedId})
+      expect(deviceId).toBe(expectedId);
+      done();
+    });
+    it('sleepEnabled test', async function(done) {
+        const intialSleepEnabled = mpu.sleepEnabled
+        const oppositeSleeEnabled = !intialSleepEnabled;
+        const newSleepEnabled = mpu.sleepEnabled;
 
-      expect(r).toBe(0x68);
+        console.log({intialSleepEnabled,oppositeSleeEnabled,newSleepEnabled})
+      expect(newSleepEnabled).toBe(oppositeSleeEnabled);
       done();
     });
   });
