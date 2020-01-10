083b6271c1baa741112e35f2b8264babdf69276c
Douglas Reuter <douglas.reuter@egov.com>
2020-01-10 10:21:32 -0600

    change test

--- src/test/mpu6050.spec.ts
+++ src/test/mpu6050.spec.ts
@@ -1,9 +1,9 @@
 import { mpu6050, mpu6050Address } from "../devices/i2c/mpu6050";
 
-describe('Serialize Service ', function() {
+describe(' mpu6050 ', function() {
    const mpu = new mpu6050(mpu6050Address.A)
     console.log('hi');
-    it('should load config', async function(done) {
+    it('getDeviceId should be 0x68', async function(done) {
         const r = mpu.getDeviceId();
 
       expect(r).toBe(0x68);
