2b1077f26577cb079b5d8248113e45604c3e8d6e
Douglas Reuter <douglas.reuter@egov.com>
2020-01-10 23:36:51 -0600

    dfd

--- src/devices/i2c/bmp280.ts
+++ src/devices/i2c/bmp280.ts
@@ -250,7 +250,7 @@ readAltitude( seaLevelhPa = 1013.25) {
        
       this.open();
         let adc_P = this.readUint24(bmp280Register.BMP280_REGISTER_PRESSUREDATA);
-        console.log({p});
+        console.log({adc_P});
         this.close();
         adc_P >>= 4;
       
